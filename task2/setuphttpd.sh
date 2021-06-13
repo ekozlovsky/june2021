ae198a71b423e373c39da7986d67b0c7d2ee4e4c
Yauhen Kazlouski <ekozlovsky@yandex.ru>
2021-06-13 14:36:54 +0300

    httpd setup script with uname

--- task2/setuphttpd.sh
+++ task2/setuphttpd.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
-
 yum update -y
 yum install -y httpd
-echo '<h1>Hello World</h1>' > /var/www/html/index.html
+os=$(uname -a)
+echo '<!DOCTYPE html>' >> /var/www/html/index.html
+echo '<html>' >> /var/www/html/index.html
+echo '<head>' >> /var/www/html/index.html
+echo '    <title>Hello World!</title>' >> /var/www/html/index.html
+echo '</head>' >> /var/www/html/index.html
+echo '<body>' >> /var/www/html/index.html
+echo '<h1>Hello World</h1>' >> /var/www/html/index.html
+echo "<h1>${os}</h1>" >> /var/www/html/index.html
+echo '</body>' >> /var/www/html/index.html
+echo '</html>' >> /var/www/html/index.html
+
 systemctl start httpd
 systemctl enable httpd
