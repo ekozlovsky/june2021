870189336eade5eba21d9c4a8ae5d0913961758f
Yauhen Kazlouski <ekozlovsky@yandex.ru>
2021-07-04 21:59:44 +0300

    setup sql comments

--- mysql/setup_database_with_control.sql
+++ mysql/setup_database_with_control.sql
@@ -30,6 +30,9 @@ CREATE TABLE results (
     CONSTRAINT `c1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+/*task contains marks 0 - no info, 1 - not done, 2 - done, 3 - done with extra */
+/*there must be ENUM instead but it must be in separate table as ech task has the same ENUM */
+
 insert  into `results`(`task1`,`task2`,`task3`,`student_id`) values 
 (0,0,0,1),
 (1,1,1,2),
@@ -38,3 +41,5 @@ insert  into `results`(`task1`,`task2`,`task3`,`student_id`) values
 (0,1,2,5),
 (1,2,3,6),
 (5,5,5,6)  on DUPLICATE KEY UPDATE task1=values(task1),task2=values(task2),task3=values(task3),student_id=student_id;
+
+/*mark 5 used for testind update on insert */
