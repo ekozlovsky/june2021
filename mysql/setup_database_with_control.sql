42086c0e3628b869d84cf9fae94243f4109db782
Yauhen Kazlouski <ekozlovsky@yandex.ru>
2021-07-04 21:49:49 +0300

    setup sql db with parameters controls

---
+++ mysql/setup_database_with_control.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE IF NOT EXISTS `students` ;
+USE `students`;
+
+DROP TABLE IF EXISTS `students`;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_id INT,
+    student_name VARCHAR(100),
+    KEY `student_id` (`student_id`)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+insert  into `students`(`student_id`,`student_name`) values 
+(1,'Ivanov'),
+(2,'Petrov'),
+(3,'Sidorov'),
+(4,'Kuznetsov'),
+(5,'Kozlov'),
+(6,'Komarov');
+
+DROP TABLE IF EXISTS `results`;
+
+CREATE TABLE results (
+    id INT AUTO_INCREMENT,
+    task1 INT,
+    task2 INT,
+    task3 INT,
+    student_id INT PRIMARY KEY,
+    KEY `id` (`id`),
+    CONSTRAINT `c1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+insert  into `results`(`task1`,`task2`,`task3`,`student_id`) values 
+(0,0,0,1),
+(1,1,1,2),
+(2,2,2,3),
+(3,3,3,4),
+(0,1,2,5),
+(1,2,3,6),
+(5,5,5,6)  on DUPLICATE KEY UPDATE task1=values(task1),task2=values(task2),task3=values(task3),student_id=student_id;
